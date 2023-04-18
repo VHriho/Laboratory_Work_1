4ecd11da075941c57548b62511aa8afd0a50e45d
VHriho <v12grig12@gmail.com>
2023-04-19 01:25:52 +0300

    Step 2

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -10,4 +10,12 @@ class StringCalculatorTest {
         assertEquals(1, StrCalculator.add("1"));
         assertEquals(3, StrCalculator.add("1,2"));
     }
+
+    @Test
+    public void Test_2(){
+        assertEquals(3, StrCalculator.add("0,1,2"));
+        assertEquals(6, StrCalculator.add("1,2,3"));
+        assertEquals(10, StrCalculator.add("1,2,3,4"));
+        assertEquals(60, StrCalculator.add("10,20,30"));
+    }
 }
